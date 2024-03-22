25820715981a11b0b13384cf041a859da14b2d29
arif1313 <arifhossen13131@gmail.com>
2024-03-23 00:57:22 +0600

    ploy add

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,35 +1,46 @@
 import TypeAnimate from "../TypeAnimate/TypeAnimate";
 import '../Home/clip.css'
+import '../Home/clip2.css'
 import pic from '../../../public/mypic.jpg';
 import Navbar from "../Navbar/Navbar";
+import cv from '../../../public/resume.pdf'
+
 const Home = () => {
     return (
-        <div className="min-h-screen overflow-hidden bg-cover relative" id="home" style={{
+        <div className="min-h-screen  bg-cover relative" id="home" style={{
             backgroundImage: `url(${pic})`,
             backgroundSize: 'cover', // Cover the entire page
             backgroundPosition: 'center', // Center the background image
             backgroundRepeat: 'no-repeat', // Do not repeat the image
-          }}>
-             <Navbar></Navbar>
-           
+        }}>
+
+            <Navbar></Navbar>
+
+
             {/* <div className=" w-1/3 flex absolute justify-center items-center  -bottom-44 left-96">
                     <div className=" h-96 w-96 bottom-0 border-8 bg-[#D8DBCB] border-[#D8DBCB]  rounded-full overflow-hidden ">
                        
                     </div>
 
                 </div> */}
+          
                 <div className="bg-[#D8DBCB] h-full w-4/5 flex absolute  items-center top-0 justify-start clip">
-                    
-                <div  className="p-5 ">
-                        
+
+                    <div className="p-5 ">
+
                         <h2 className="text-2xl font-bold text-black ">Hello I am .....</h2>
                         <h1 className="text-5xl font-bold mt-5"><span className="text-[#55b1fb]">MD Arif hossen</span> </h1>
                         <h2 className="mt-5 text-black font-bold"><TypeAnimate></TypeAnimate></h2>
-                        
-                    </div>
-                    
 
-                </div>
+                    </div>
+                    <div className="navbar-end">
+                        <a href={cv} download='Md Arif resume' className="btn">Download cv</a>
+                    </div>
+                   
+              
+
+
+            </div>
         </div>
     );
 };
